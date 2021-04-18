e2a2f2b87dfe5ca08b1423afc60ce6af9e7c9eaa
Asad <71043942+asdhamidi@users.noreply.github.com>
2021-04-19 01:47:28 +0530

    Adding Misc Files

--- Misc/Lecture8/lect8.py
+++ Misc/Lecture8/lect8.py
@@ -1,23 +1,23 @@
-import random, matplotlib.pyplot as pylab, numpy
+import random, matplotlib.pylab as pylab, numpy
 
-set line width
-pylab.rcParams['lines.linewidth'] = 4
-set font size for titles
-pylab.rcParams['axes.titlesize'] = 20
-set font size for labels on axes
-pylab.rcParams['axes.labelsize'] = 20
-set size of numbers on x-axis
-pylab.rcParams['xtick.labelsize'] = 16
-set size of numbers on y-axis
-pylab.rcParams['ytick.labelsize'] = 16
-set size of ticks on x-axis
-pylab.rcParams['xtick.major.size'] = 7
-set size of ticks on y-axis
-pylab.rcParams['ytick.major.size'] = 7
-set size of markers
-pylab.rcParams['lines.markersize'] = 10
-set number of examples shown in legends
-pylab.rcParams['legend.numpoints'] = 1
+# set line width
+# pylab.rcParams['lines.linewidth'] = 4
+# set font size for titles
+# pylab.rcParams['axes.titlesize'] = 20
+# set font size for labels on axes
+# pylab.rcParams['axes.labelsize'] = 20
+# set size of numbers on x-axis
+# pylab.rcParams['xtick.labelsize'] = 16
+# set size of numbers on y-axis
+# pylab.rcParams['ytick.labelsize'] = 16
+# set size of ticks on x-axis
+# pylab.rcParams['xtick.major.size'] = 7
+# set size of ticks on y-axis
+# pylab.rcParams['ytick.major.size'] = 7
+# set size of markers
+# pylab.rcParams['lines.markersize'] = 10
+# set number of examples shown in legends
+# pylab.rcParams['legend.numpoints'] = 1
 
 
 def makeHist(data, title, xlabel, ylabel, bins = 20):
--- play.py
+++ play.py
@@ -1,41 +1,43 @@
 # This file is for trying random codes.
 
-import random
-import matplotlib.pyplot as plt
-def CLT(size):
-    population = []
-    for r in range(10000):
-        population.append(random.randint(1, 10000))
+# import random
+# import matplotlib.pyplot as plt
+# def CLT(size):
+#     population = []
+#     for r in range(10000):
+#         population.append(random.randint(1, 10000))
 
-    samples = []
-    for r in range(500):
-        currentSample = []
-        for i in range(size):
-            currentSample.append(random.choice(population))
-        samples.append(currentSample)
+#     samples = []
+#     for r in range(500):
+#         currentSample = []
+#         for i in range(size):
+#             currentSample.append(random.choice(population))
+#         samples.append(currentSample)
 
-    means = []
-    for sample in samples:
-        means.append(sum(sample) / size)
+#     means = []
+#     for sample in samples:
+#         means.append(sum(sample) / size)
 
-    return (sum(population) / len(population), sum(means) / len(means))
+#     return (sum(population) / len(population), sum(means) / len(means))
 
-size = []
-popMean = []
-meanOfMeans = []
-for r in range(100, 10000, 100):
-    size.append(r)
-    pMean, mMean = CLT(r)
-    popMean.append(pMean)
-    meanOfMeans.append(mMean)
-diff = []
-zero = []
-for r in range(len(popMean)):
-    diff.append(popMean[r] - meanOfMeans[r])
-    zero.append(0)
+# size = []
+# popMean = []
+# meanOfMeans = []
+# for r in range(100, 10000, 100):
+#     size.append(r)
+#     pMean, mMean = CLT(r)
+#     popMean.append(pMean)
+#     meanOfMeans.append(mMean)
+# diff = []
+# zero = []
+# for r in range(len(popMean)):
+#     diff.append(popMean[r] - meanOfMeans[r])
+#     zero.append(0)
 
-plt.plot(size, diff, "r-.")
-plt.plot(size, zero, "k-")
-plt.xlabel("Sample Size")
-plt.ylabel("Difference in Pop. Mean and Mean of Means.")
-plt.show()
+# plt.plot(size, diff, "r-.")
+# plt.plot(size, zero, "k-")
+# plt.xlabel("Sample Size")
+# plt.ylabel("Difference in Pop. Mean and Mean of Means.")
+# plt.show()
+
+
